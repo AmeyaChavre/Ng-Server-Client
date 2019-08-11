ad9a1d8a684eda741c4cf00dc402f7f5c47fe473
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-11 14:06:29 +0530

    Commit#15

--- ng8-app/src/app/app.module.ts
+++ ng8-app/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -11,7 +12,8 @@ import { ServerComponent } from './server/server.component'; // importing the se
     ServerComponent // registering the server component 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent] // bootstarping the AppComponent 
