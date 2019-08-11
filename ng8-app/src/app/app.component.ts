5c606c1906627aa03c02f91f08226ab9778f3cda
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-12 00:23:29 +0530

    Commit#29

--- ng8-app/src/app/app.component.ts
+++ ng8-app/src/app/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css'] 
+  /*
+  Below we are using inline styles , both styleUrls and styles arrays cannot be present in the same component 
+  */
+  styles: [`
+h3 {
+	color:red; 
+}
+  `]
 })
 export class AppComponent {
   title = 'ng8-app';
