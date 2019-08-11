296edd04e2726c2b577bace21f46d7e2c4b5a4e7
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-11 23:09:18 +0530

    Commit#25

--- ng8-app/src/app/servers/servers.component.ts
+++ ng8-app/src/app/servers/servers.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `<app-server></app-server>
+  <hr>
+  <p>Nesting Components without using templateUrl Property</p>
+  <hr>
+  <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
