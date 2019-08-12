821741c397b148df7745df6bfb57ae0d3d68af59
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-12 13:20:11 +0530

    Commit#40

--- ng8-app/src/app/servers/servers.component.ts
+++ ng8-app/src/app/servers/servers.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   // selector: app-servers,
   // selector: '[app-servers]', 
-  selector: '.app-servers',
+  // selector: '.app-servers',
   template: `<app-server></app-server>
   <hr>
   <p>Nesting Components without using templateUrl Property - INLINE TEMPLATES</p>
