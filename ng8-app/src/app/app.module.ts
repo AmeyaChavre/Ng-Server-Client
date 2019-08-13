6a5ec807fc42eb665cc29244a05429b618d0e833
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-13 13:01:53 +0530

    Commit#44

--- ng8-app/src/app/app.module.ts
+++ ng8-app/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component'; // importing the servers component in root module
-
+import { ClientComponent } from './client/client.component';
 
 @NgModule({
   declarations: [
