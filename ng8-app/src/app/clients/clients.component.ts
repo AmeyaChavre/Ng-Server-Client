04a9d3f7300766723cbc494ed23261a6a4a4bf10
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-15 13:33:15 +0530

    Commit#59

--- ng8-app/src/app/clients/clients.component.ts
+++ ng8-app/src/app/clients/clients.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-clients',
-  template:`
-	<app-client></app-client>
-	<app-client></app-client>
-  `,
+  template:`<app-client></app-client>
+  <hr>
+  <p>Nesting Components without using templateUrl Property - INLINE TEMPLATES</p>
+  <hr>
+  <app-client></app-client>`,
   styleUrls: ['./clients.component.css']
 })
 export class ClientsComponent implements OnInit {
