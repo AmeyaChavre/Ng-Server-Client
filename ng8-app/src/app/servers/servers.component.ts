77247dc4e428df8aeb7584e2520e975f77fbbb60
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-15 21:37:11 +0530

    Commit#74

--- ng8-app/src/app/servers/servers.component.ts
+++ ng8-app/src/app/servers/servers.component.ts
@@ -4,9 +4,10 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers', // the most preferrable style for using selector prperty in components
   // selector: '[app-servers]', 
   // selector: '.app-servers',
-  template: `<app-server></app-server>
-  <hr>
-  <app-server></app-server>`,
+  //template: `<app-server></app-server>
+  //<hr>
+  //<app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
