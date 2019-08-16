f4a75fa12a5c523bae5011284d4048ecf0c0bed9
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-16 14:27:09 +0530

    Commit#106

--- ng8-app/src/app/servers/servers.component.ts
+++ ng8-app/src/app/servers/servers.component.ts
@@ -30,7 +30,7 @@ export class ServersComponent implements OnInit {
   }
 
   onUpdateServerName(event: Event){
-//    console.log(event);
+      console.log(event);
       this.serverName=(<HTMLInputElement>event.target).value;
   }
 
