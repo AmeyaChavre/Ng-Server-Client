38b741c5dde1599d74ca117591dbccd7ca8709c2
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-18 00:08:21 +0530

    Commit#133

--- ng8-app/src/app/servers/servers.component.ts
+++ ng8-app/src/app/servers/servers.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers', // the most preferrable style for using selector prperty in components
+  selector: 'app-servers', // the most preferrable style for using selector property in components
   // selector: '[app-servers]', 
   // selector: '.app-servers',
   //template: `<app-server></app-server>
