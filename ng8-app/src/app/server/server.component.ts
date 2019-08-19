a45857fce7ea30e8cafc6986b35a3c3d3068d666
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-19 21:08:29 +0530

    Commit#200

--- ng8-app/src/app/server/server.component.ts
+++ ng8-app/src/app/server/server.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
     selector: 'app-server',
     templateUrl: './server.component.html',
    styles : [`
-			.online{}
+			.online{
+				
+			}
 		   `]
 })
 
