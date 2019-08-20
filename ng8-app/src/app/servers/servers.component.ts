460f15ad83ec141aad916db1c631792602d1e30b
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-20 20:28:13 +0530

    Commit#246

--- ng8-app/src/app/servers/servers.component.ts
+++ ng8-app/src/app/servers/servers.component.ts
@@ -16,7 +16,7 @@ export class ServersComponent implements OnInit {
   serverName = 'Testserver'; // default server will be "Testserver"
   userName = ''; 
   serverCreated:boolean = false; // default value should be false
-  servers =
+  servers = [];
   constructor() { 
     setTimeout(()=>{
       this.allowNewServer = true;
