f8491ddd320ed9c57bb650171c7e1dbb0d4b6f34
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-22 14:28:14 +0530

    Commit#315

---
+++ ng8-app/src/app/passcode/passcode.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PasscodeComponent } from './passcode.component';
+
+describe('PasscodeComponent', () => {
+  let component: PasscodeComponent;
+  let fixture: ComponentFixture<PasscodeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PasscodeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PasscodeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
