2b9b69472eea473bd41376e8225412c146dc8427
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-23 02:20:04 +0530

    Commit#350

--- ng8-app/src/app/servers/servers.component.ts
+++ ng8-app/src/app/servers/servers.component.ts
@@ -54,6 +54,7 @@ export class ServersComponent implements OnInit {
    passcodeGenerator(){
       this.passcodeReveal = !this.passcodeReveal;
       //this.log.push(this.log.length + 1);
+      this.log.push(new Date());
    }
 
 }
